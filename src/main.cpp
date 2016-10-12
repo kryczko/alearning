93aed90dc6fe76d36ac753efcedfff4aa56a783a
kryczko <kevinryczko@me.com>
2016-10-12 17:22:47 -0400

    added rng and command line parser in the C++ code

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <thread>
+#include <boost/program_options.hpp> 
+
+#include "gen.h"
+#include "argparser.h"
+
+using namespace std;
+
+namespace po = boost::program_options;
+
+int main(int argc, char ** argv) {
+	ArgParser parser(argc, argv);
+	int n_atoms = 108;
+	Generator g(n_atoms, parser);
+	return 0;
+}
