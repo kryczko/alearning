714d870fe4fd3afb1488239fdac3872081981efe
kryczko <kevinryczko@me.com>
2016-10-14 17:09:43 -0400

    added another check function to see when the MC stabalizes

--- src/gen.h
+++ src/gen.h
@@ -17,43 +17,52 @@ namespace fs = boost::filesystem;
 using namespace std;
 
 class Generator {
-	private:
-		vector<vector <double>> lattice;
-		vector<vector<vector <double>>> md;
-		vector<int> defect_indices, dopant_indices, z;
-		vector<string> dopants;
+    private:
+        vector<vector <double>> lattice;
+        vector<vector<vector <double>>> md;
+        vector<int> defect_indices, dopant_indices, z;
+        vector<string> dopants;
 
-		int n_atoms, n_dopants, n_defects, seed;
-	public:
+        int n_atoms, n_dopants, n_defects, seed;
+    public:
         Rng rng;
         double etarget;
 
-		void readXyzFile() {
-			ifstream xyzfile;
-			xyzfile.open("static/Al_MD.xyz");
-			string stuff;
-			int time_steps = 0;
-			int local_count = 0;
-			vector<double> coords(3);
-			vector<vector<double>> time_step;
-			while ( !xyzfile.eof() ) {
-				xyzfile >> stuff;
-				if ( stuff == "Al") {
-					xyzfile >> coords[0] >> coords[1] >> coords[2];
-					time_step.push_back(coords);
-					local_count ++;
-					if (local_count % n_atoms == 0) {
-						md.push_back(time_step);
-						time_steps ++;
-					}
-				}
-			}
-		}
+        void readXyzFile() {
+            ifstream xyzfile;
+            xyzfile.open("static/Al_MD.xyz");
+            string stuff;
+            int time_steps = 0;
+            int local_count = 0;
+            vector<double> coords(3);
+            vector<vector<double>> time_step;
+            while ( !xyzfile.eof() ) {
+                xyzfile >> stuff;
+                if ( stuff == "Al") {
+                    xyzfile >> coords[0] >> coords[1] >> coords[2];
+                    time_step.push_back(coords);
+                    local_count ++;
+                    if (local_count % n_atoms == 0) {
+                        md.push_back(time_step);
+                        time_steps ++;
+                    }
+                }
+            }
+        }
 
-		void setLattice() {
-			int rnd_time_step = this->rng.randint(0, this->md.size());
-			this->lattice = md[rnd_time_step];
-		}
+        void setSeed(int seed) {
+            this->seed = seed;
+            this->rng = Rng(seed);
+
+        }
+        int getSeed() {
+            return this->seed;
+        }
+
+        void setLattice() {
+            int rnd_time_step = this->rng.randint(0, this->md.size());
+            this->lattice = md[rnd_time_step];
+        }
 
         void printDopantAndDefectIndices() {
             cout << "Defects: ";
@@ -67,46 +76,46 @@ class Generator {
             cout << "\n";
         }
 
-		void printLattice() {
-			for (auto& coords : this->lattice) {
-				cout << coords[0] << "\t" << coords[1] << "\t" << coords[2] << "\n";
-			}
-		}
+        void printLattice() {
+            for (auto& coords : this->lattice) {
+                cout << coords[0] << "\t" << coords[1] << "\t" << coords[2] << "\n";
+            }
+        }
 
-		void placeDefectsAndDopants() {
+        void placeDefectsAndDopants() {
             this->dopant_indices.clear();
             this->defect_indices.clear();
-			int total = this->n_dopants + this->n_defects;
-			for (int i = 0; i < total; i ++) {
+            int total = this->n_dopants + this->n_defects;
+            for (int i = 0; i < total; i ++) {
                 int rand_index = -1;
-				bool in_dopant_vector = true;
-				bool in_defect_vector = true;
-				while (in_dopant_vector && in_defect_vector) {
-					rand_index = this->rng.randint(0, this->n_atoms);
-					in_dopant_vector = find(this->dopant_indices.begin(), this->dopant_indices.end(), rand_index) != this->dopant_indices.end();
-					in_defect_vector = find(this->defect_indices.begin(), this->defect_indices.end(), rand_index) != this->defect_indices.end();
-				}
+                bool in_dopant_vector = true;
+                bool in_defect_vector = true;
+                while (in_dopant_vector && in_defect_vector) {
+                    rand_index = this->rng.randint(0, this->n_atoms);
+                    in_dopant_vector = find(this->dopant_indices.begin(), this->dopant_indices.end(), rand_index) != this->dopant_indices.end();
+                    in_defect_vector = find(this->defect_indices.begin(), this->defect_indices.end(), rand_index) != this->defect_indices.end();
+                }
                 if (i < this->n_dopants) {
                     this->dopant_indices.push_back(rand_index);
                 } else {
                     this->defect_indices.push_back(rand_index);
                 }
-			}
-		}
+            }
+        }
 
-		void printConfig() {
-			cout << "\nCONFIG INFO:\n";
-			cout << "------------\n";
-			cout << "Number of atoms: " << this->n_atoms << endl;
-			cout << "Total number of dopant atoms: " << this->n_dopants << endl;
-			for (int i = 0; i < this->dopants.size() / 2; i ++) {
-				cout << "  Dopant " << i << " atom type: " << this->dopants[2*i] << endl;
-				cout << "  Number of dopant atoms: " << this->dopants[2*i + 1] << endl;
-			}
-			cout << "Number of defects: " << this->n_defects << endl;
-			cout << "Energy target: " << this->etarget << endl;
-			cout << "Random seed: " << this->seed << endl;
-		}
+        void printConfig() {
+            cout << "\nCONFIG INFO:\n";
+            cout << "------------\n";
+            cout << "Number of atoms: " << this->n_atoms << endl;
+            cout << "Total number of dopant atoms: " << this->n_dopants << endl;
+            for (int i = 0; i < this->dopants.size() / 2; i ++) {
+                cout << "  Dopant " << i << " atom type: " << this->dopants[2*i] << endl;
+                cout << "  Number of dopant atoms: " << this->dopants[2*i + 1] << endl;
+            }
+            cout << "Number of defects: " << this->n_defects << endl;
+            cout << "Energy target: " << this->etarget << endl;
+            cout << "Random seed: " << this->seed << endl;
+        }
 
         double totalCoulombEnergy() {
             vector<int> z(this->n_atoms, 13); // 13 for Al
@@ -141,6 +150,16 @@ class Generator {
             if (!total) {
                 return;
             }
+            vector<vector<int>> split_dopants;
+            int dopant_count = 0;
+            for (int i = 0; i < this->dopants.size() / 2; i ++) {
+                vector<int> dopant_indices;
+                for (int j = dopant_count; j < stoi(this->dopants[i*2 + 1]) + dopant_count; j ++) {
+                    dopant_indices.push_back(this->dopant_indices[j]);
+                }
+                dopant_count += stoi(this->dopants[i*2 + 1]);
+                split_dopants.push_back(dopant_indices);
+            }
             vector<int> all_indices;
             for (int val : this->defect_indices) {
                 all_indices.push_back(val);
@@ -152,7 +171,7 @@ class Generator {
             int choice = all_indices[rand_int];
             string typeof = "null";
             vector<int> ignored_indices;
-            if (rand_int < this->defect_indices.size()) {
+            if (rand_int <= this->defect_indices.size() && this->defect_indices.size() != 0) {
                 typeof = "defect";
                 ignored_indices = this->defect_indices;
             } else {
@@ -161,20 +180,20 @@ class Generator {
                 for (int i = 0; i < this->dopants.size() / 2; i ++) {
                     if (new_rand_int - stoi(this->dopants[i * 2 + 1]) < 0) {
                         for (int j = new_rand_int; j < stoi(this->dopants[i * 2 + 1]); j ++ ) {
-                            ignored_indices.push_back(this->defect_indices[j]);
+                            ignored_indices = split_dopants[i];
                         }
                         break;
                     }
-                    new_rand_int -= stoi(this->dopants[i * 2 - 1]);
+                    new_rand_int -= stoi(this->dopants[i * 2 + 1]);
                 }
             }
-
             bool valid_choice = false;
             int rand_atom = -1;
             while (!valid_choice) {
                 rand_atom = this->rng.randint(0, this->n_atoms);
                 valid_choice = find(ignored_indices.begin(), ignored_indices.end(), rand_atom) == ignored_indices.end();
             }
+
             if (typeof == "defect") {
                 auto it = find(this->defect_indices.begin(), this->defect_indices.end(), choice);
                 int index = distance(this->defect_indices.begin(), it);
@@ -184,6 +203,29 @@ class Generator {
                 int index = distance(this->dopant_indices.begin(), it);
                 this->dopant_indices[index] = rand_atom;
             }
+        }
+
+        double areaOfDopantsAndDefects() {
+            vector<int> all_indices;
+            for (int val : this->defect_indices) {
+                all_indices.push_back(val);
+            }
+            for (int val: this->dopant_indices) {
+                all_indices.push_back(val);
+            }
+            vector<double> distances;
+            for (int i = 0; i < all_indices.size(); i ++) {
+                for (int j = 0; j < all_indices.size(); j ++) {
+                    if (i != j) {
+                        distances.push_back(r(this->lattice[all_indices[i]], this->lattice[all_indices[j]]));
+                    }
+                }
+            }
+            double area = distances[0];
+            for (int i = 1; i < distances.size(); i ++) {
+                area *= distances[i];
+            }
+            return sqrt(area);
         }
 
         void writeLattice(int lattice_ind) {
@@ -202,7 +244,7 @@ class Generator {
                 counts += dopants[2*i + 1] + "  ";
             }
             output << names << "\n";
-            output << counts << "\n";
+            output << counts << "\nCartesian\n";
             for (int i = 0; i < this->lattice.size(); i ++) {
                 bool in_dopants = find(this->dopant_indices.begin(), this->dopant_indices.end(), i) != this->dopant_indices.end();
                 bool in_defects = find(this->defect_indices.begin(), this->defect_indices.end(), i) != this->defect_indices.end();
@@ -218,23 +260,23 @@ class Generator {
 
         }
 
-		Generator(int n_atoms, ArgParser parser) {
-			this->n_atoms = n_atoms;
-			this->n_defects = parser["defects"].as<int>();
-			this->seed = parser["seed"].as<int>();
-			this->dopants = parser["dopants"].as<vector<string>>();
-			this->n_dopants = 0;
-			for (int i = 0; i < this->dopants.size() / 2; i ++) {
-				this->n_dopants += stoi(this->dopants[2*i + 1]);
-			}
-			this->etarget = parser["etarget"].as<double>();
-			this->rng = Rng(parser["seed"].as<int>());
+        Generator(int n_atoms, ArgParser parser) {
+            this->n_atoms = n_atoms;
+            this->n_defects = parser["defects"].as<int>();
+            this->seed = parser["seed"].as<int>();
+            this->dopants = parser["dopants"].as<vector<string>>();
+            this->n_dopants = 0;
+            for (int i = 0; i < this->dopants.size() / 2; i ++) {
+                this->n_dopants += stoi(this->dopants[2*i + 1]);
+            }
+            this->etarget = parser["etarget"].as<double>();
+            this->rng = Rng(parser["seed"].as<int>());
             this->printConfig();
 
-			this->readXyzFile();
-			this->setLattice();
+            this->readXyzFile();
+            this->setLattice();
             this->placeDefectsAndDopants();
-		} 
+        } 
 };
 
 
